12467f76a6c80a7956f6d1f1d47a17680267ca89
juksalmi <pekka@jukka-pekkas-MacBook-Air.local>
2015-03-09 10:59:17 +0200

    jps filet lisatty

---
+++ jps.c
@@ -0,0 +1,9 @@
+//
+//  jps.c
+//  
+//
+//  Created by jukka-pekka salminen on 09/03/15.
+//
+//
+
+#include "jps.h"
---
+++ jps.h
@@ -0,0 +1,14 @@
+//
+//  jps.h
+//  
+//
+//  Created by jukka-pekka salminen on 09/03/15.
+//
+//
+
+#ifndef ____jps__
+#define ____jps__
+
+#include <stdio.h>
+
+#endif /* defined(____jps__) */
